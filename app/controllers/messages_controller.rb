33533a09195de7e25531f36262c6b3d59654c1af
Laura Steadman <lauramsteadman@gmail.com>
2013-06-13 18:29:20 -0600

    testing validations for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,10 @@ class MessagesController  < ApplicationController
 
   def create
     @message = Message.create(params[:message])
+
+    respond_to do |format|
+      format.html { redirect_to chat_path(@message.chat_id)}
+      format.js {@message}
+    end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
   attr_accessible :content, :chat_id
 
+  validates :content, :chat_id, :presence => true
   belongs_to :chat
 end
