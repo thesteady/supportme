e51aad84946e56f26ffa89f970ef4fa8bdf8c72c
Laura Steadman <lauramsteadman@gmail.com>
2013-06-24 13:34:26 -0600

    JavaScript please be my friend

--- app/assets/javascripts/chats.js
+++ app/assets/javascripts/chats.js
@@ -1,6 +1,27 @@
 var WaitingClickListener = function() {
+    
     $('.waiting').on('click', function() {
-      $('#active-chats ul').append(this).append('<br>');
+      // create a new list item in the active tab
+      // add the link (this) to the list item ive just created using inner html
+      console.log(this);
+
+      var linkBeingMoved = this;
+
+      var newListItem = document.createElement('li');
+      newListItem.setAttribute('id', 'testing')
+      newListItem.innerHTML = linkBeingMoved;
+
+      var listActive = document.getElementById('active-chats-list');
+      var listWaiting = document.getElementById('waiting-chats-list');
+      console.log(listActive);
+      console.log(listWaiting);
+
+
+      $("#active-chats-list").append(this).append("<br>");
+
+
+
+     // $('#active-chats ul').append(this).append('<br>');
     });
   };
 
