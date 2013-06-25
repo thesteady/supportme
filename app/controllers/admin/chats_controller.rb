8cacf0d473cc161f910f9aacb1b6a6e0faee5e60
Laura Steadman <lauramsteadman@gmail.com>
2013-06-25 15:11:51 -0600

    Cleanup

--- app/controllers/admin/chats_controller.rb
+++ app/controllers/admin/chats_controller.rb
@@ -6,8 +6,8 @@ class Admin::ChatsController < ApplicationController
   end
 
   def show
-    require_customer
-
+    #require_customer
+    
     @chat = Chat.find(params[:id])
     @chat.update_attributes(status: 'active') if @chat.status == 'waiting'
     @messages = @chat.messages
